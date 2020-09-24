d571becad58429fa1aca5d13713bcc57335d89a3
ahsan-codejit <ahsan@codejit.com>
2020-09-24 05:39:35 +0200

    User Provider implementation using context api feature to maintain user data state

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import UserProvider from './providers/UserProvider';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <UserProvider>
+    <Router>
+      <App />
+    </Router>
+  </UserProvider>,
   document.getElementById('root')
 );
 
